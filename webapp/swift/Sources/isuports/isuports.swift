664b914ae067a31d5f4a640a201943ffa97e0508
kenta okamura <side.junktown@gmail.com>
2022-08-20 23:34:17 +0900

    全エンドポイントのルーティング設定

--- webapp/swift/Sources/isuports/isuports.swift
+++ webapp/swift/Sources/isuports/isuports.swift
@@ -10,7 +10,7 @@ let initializeScript = "../sql/init.sh"
 let cookieName = "isuports_session"
 let tenantNameRegexp = #/^[a-z][a-z0-9-]{0,61}[a-z0-9]$/#
 
-enum Role: String {
+enum Role: String, Codable {
     case admin
     case organizer
     case player
@@ -64,6 +64,90 @@ final class CacheControlPrivateMiddleware: Middleware {
             return response
         }
     }
+}
+
+@main struct Main {
+    static func main() async throws {
+        LoggingSystem.bootstrap { label in
+            var handler = ConsoleLogger(label: label, console: Terminal())
+            handler.logLevel = .debug
+            return handler
+        }
+        
+        let (pool, shutdown) = connectAdminDB()
+        defer { try! shutdown.close() }
+        
+        let threadPool = NIOThreadPool(numberOfThreads: 16)
+        threadPool.start()
+        defer { try! threadPool.syncShutdownGracefully() }
+        
+        let app = try Application(.detect())
+        defer { app.shutdown() }
+        
+        app.http.server.configuration.port = Int(getEnv(key: "SERVER_APP_PORT", defaultValue: "3000"))!
+        app.middleware.use(CacheControlPrivateMiddleware())
+        
+        func route(_ route: @escaping @Sendable (Handler.Type) -> ((Handler) -> () async throws -> Response)) -> (Request) async throws -> Response {
+            { request in
+                let handler = Handler(
+                    request: request,
+                    adminDB: pool.database(logger: request.logger),
+                    threadPool: threadPool,
+                    eventLoopGroup: request.eventLoop
+                )
+                return try await route(Handler.self)(handler)()
+            }
+        }
+        
+        // SaaS管理者向けAPI
+        app.post("/api/admin/tenants/add", use: route { $0.tenantsAdd })
+        app.get("/api/admin/tenants/billing", use: route { $0.tenantsBilling })
+
+        // テナント管理者向けAPI - 参加者追加、一覧、失格
+        app.get("/api/organizer/players", use: route { $0.playersList })
+        app.post("/api/organizer/players/add", use: route { $0.playersAdd })
+        app.post("/api/organizer/player/:player_id/disqualified", use: route { $0.playerDisqualified })
+
+        // テナント管理者向けAPI - 大会管理
+        app.post("/api/organizer/competitions/add", use: route { $0.competitionsAdd })
+        app.post("/api/organizer/competition/:competition_id/finish", use: route { $0.competitionFinish })
+        app.post("/api/organizer/competition/:competition_id/score", use: route { $0.competitionScore })
+        app.get("/api/organizer/billing", use: route { $0.billing })
+        app.get("/api/organizer/competitions", use: route { $0.organizerCompetitions })
+
+        // 参加者向けAPI
+        app.get("/api/player/player/:player_id", use: route { $0.player })
+        app.get("/api/player/competition/:competition_id/ranking", use: route { $0.competitionRanking })
+        app.get("/api/player/competitions", use: route { $0.playerCompetitions })
+
+        // 全ロール及び未認証でも使えるhandler
+        app.get("/api/me", use: route { $0.me })
+
+        // ベンチマーカー向けAPI
+        app.post("/initialize", use: route { $0.initialize })
+        
+        try app.start()
+        try await app.running?.onStop.get()
+    }
+}
+
+struct Empty: Encodable {}
+
+struct SuccessResult<T: Encodable>: Encodable {
+    let status: Bool = true
+    var data: T?
+    
+    init(data: T) {
+        self.data = data
+    }
+    init() where T == Empty {
+        self.data = nil
+    }
+}
+
+struct FailureResult: Encodable  {
+    let status: Bool = false
+    var message: String
 }
 
 struct Handler {
@@ -136,16 +220,6 @@ struct Handler {
         throw lastError ?? StringError("unexpected")
     }
     
-    struct SuccessResult<T: Encodable>: Encodable {
-        var status: Bool
-        var data: T?
-    }
-
-    struct FailureResult: Encodable  {
-        var status: Bool
-        var message: String
-    }
-
     // アクセスしてきた人の情報
     struct Viewer {
         var role: Role
@@ -181,7 +255,7 @@ struct Handler {
         } catch {
             throw Abort(.unauthorized, reason: "error signer.verify: \(error)")
         }
-
+        
         guard let tokenSub = token.sub else {
             throw Abort(.unauthorized, reason: "invalid token: subject is not found in token: \(tokenString)")
         }
@@ -202,7 +276,7 @@ struct Handler {
         if tenant.name == "admin" && role != .admin {
             throw Abort(.unauthorized, reason: "tenant not found")
         }
-
+        
         guard tenant.name == tokenAud[0] else {
             throw Abort(.unauthorized, reason: "invalid token: tenant name is not match with \(request.url.host ?? ""): \(tokenString)")
         }
@@ -215,14 +289,14 @@ struct Handler {
         let baseHost = getEnv(key: "ISUCON_BASE_HOSTNAME", defaultValue: ".t.isucon.dev")
         let host = request.url.host ?? ""
         let tenantName = host.hasSuffix(baseHost)
-            ? String(host[host.startIndex ..< host.index(host.endIndex, offsetBy: -baseHost.count)])
-            : host
+        ? String(host[host.startIndex ..< host.index(host.endIndex, offsetBy: -baseHost.count)])
+        : host
         
         // SaaS管理者用ドメイン
         if tenantName == "admin" {
             return TenantRow(id: 0, name: "admin", display_name: "admin", created_at: 0, updated_at: 0)
         }
-
+        
         // テナントの存在確認
         return try await adminDB.sql().execute(
             "SELECT * FROM tenant WHERE name = \(bind: tenantName)"
@@ -298,7 +372,7 @@ struct Handler {
         return RelativePath(tenantDBDir)
             .appending(component: "\(id).lock")
     }
-
+    
     // 排他ロックする
     func flockByTenantID<T>(tenantID: Int64, _ body: () throws -> T) throws -> T {
         let p = lockFilePath(id: tenantID)
@@ -316,8 +390,8 @@ struct Handler {
     // SasS管理者用API
     // テナントを追加する
     // POST /api/admin/tenants/add
-    func tenantsAdd() async throws -> TenantsAddResult {
-        fatalError("TODO")
+    func tenantsAdd() async throws -> Response {
+        return try .json(content: SuccessResult())
     }
     
     // テナント名が規則に沿っているかチェックする
@@ -328,55 +402,233 @@ struct Handler {
         throw StringError("invalid tenant name: \(name)")
     }
     
-    struct TenantWithBilling: Encodable  {
+    struct BillingReport: Encodable {
+        var competition_id: String
+        var competition_title: String
+        var player_count: Int64        // スコアを登録した参加者数
+        var visitor_count: Int64       // ランキングを閲覧だけした(スコアを登録していない)参加者数
+        var billing_player_yen: Int64  // 請求金額 スコアを登録した参加者分
+        var billing_visitor_yen: Int64 // 請求金額 ランキングを閲覧だけした(スコアを登録していない)参加者分
+        var billing_yen: Int64         // 合計請求金額
+    }
+    
+    struct VisitHistoryRow: Decodable {
+        var player_id: String
+        var tenant_id: Int64
+        var competition_id: String
+        var created_at: Int64
+        var updated_at: Int64
+    }
+    
+    struct VisitHistorySummaryRow: Decodable {
+        var player_id: String
+        var min_created_at: Int64
+    }
+    
+    // 大会ごとの課金レポートを計算する
+    func billingReportByCompetition(tenantDB: some SQLDatabase, tenantID: Int64, competitonID: String) async throws -> BillingReport {
+        fatalError("TODO")
+    }
+    
+    struct TenantWithBilling: Encodable {
         var id: String
         var name: String
         var display_name: String
         var billing: Int64
     }
-}
+    
+    struct TenantsBillingResult: Encodable {
+        var tenants: [TenantWithBilling]
+    }
+    
+    // SaaS管理者用API
+    // テナントごとの課金レポートを最大10件、テナントのid降順で取得する
+    // GET /api/admin/tenants/billing
+    // URL引数beforeを指定した場合、指定した値よりもidが小さいテナントの課金レポートを取得する
+    func tenantsBilling() async throws -> Response {
+        return try .json(content: SuccessResult())
+    }
+    
+    struct PlayerDetail: Encodable {
+        var id: String
+        var display_name: String
+        var is_disqualified: Bool
+    }
+    
+    struct PlayersListResult: Encodable {
+        var players: [PlayerDetail]
+    }
+    
+    // テナント管理者向けAPI
+    // GET /api/organizer/players
+    // 参加者一覧を返す
+    func playersList() async throws -> Response {
+        return try .json(content: SuccessResult())
+    }
+    
+    struct PlayersAddResult: Encodable {
+        var players: PlayerDetail
+    }
+    
+    // テナント管理者向けAPI
+    // GET /api/organizer/players/add
+    // テナントに参加者を追加する
+    func playersAdd() async throws -> Response {
+        return try .json(content: SuccessResult())
+    }
+    
+    struct PlayerDisqualifiedResult: Encodable {
+        var player: PlayerDetail
+    }
+    
+    // テナント管理者向けAPI
+    // POST /api/organizer/player/:player_id/disqualified
+    // 参加者を失格にする
+    func playerDisqualified() async throws -> Response {
+        return try .json(content: SuccessResult())
+    }
+    
+    struct CompetitionDetail: Encodable {
+        var id: String
+        var title: String
+        var is_finished: Bool
+    }
+    
+    struct CompetitionsAddResult: Encodable {
+        var competition: CompetitionDetail
+    }
+    
+    // テナント管理者向けAPI
+    // POST /api/organizer/competitions/add
+    // 大会を追加する
+    func competitionsAdd() async throws -> Response {
+        return try .json(content: SuccessResult())
+    }
+    
+    // テナント管理者向けAPI
+    // POST /api/organizer/competition/:competition_id/finish
+    // 大会を終了する
+    func competitionFinish() async throws -> Response {
+        return try .json(content: SuccessResult())
+    }
+    
+    struct ScoreResult: Encodable {
+        var rows: Int64
+    }
+    
+    // テナント管理者向けAPI
+    // POST /api/organizer/competition/:competition_id/score
+    // 大会のスコアをCSVでアップロードする
+    func competitionScore() async throws -> Response {
+        return try .json(content: SuccessResult())
+    }
+    
+    struct BillingResult: Encodable {
+        var reports: [BillingReport]
+    }
+    
+    // テナント管理者向けAPI
+    // GET /api/organizer/billing
+    // テナント内の課金レポートを取得する
+    func billing() async throws -> Response {
+        return try .json(content: SuccessResult())
+    }
+    
+    struct PlayerScoreDetail: Encodable {
+        var competition_title: String
+        var score: Int64
+    }
+    
+    struct PlayerResult: Encodable {
+        var player: PlayerDetail
+        var scores: [PlayerScoreDetail]
+    }
+    
+    // 参加者向けAPI
+    // GET /api/player/player/:player_id
+    // 参加者の詳細情報を取得する
+    func player() async throws -> Response {
+        return try .json(content: SuccessResult())
+    }
+    
+    struct CompetitionRank: Encodable {
+        var rank: Int64
+        var score: Int64
+        var player_id: String
+        var player_display_name: String
+        var rowNum: Int64 // APIレスポンスのJSONには含まれない
+        
+        enum CodingKeys: CodingKey {
+            case rank
+            case score
+            case player_id
+            case player_display_name
+        }
+    }
+    
+    struct CompetitionRankingResult: Encodable {
+        var competition: CompetitionDetail
+        var ranks: [CompetitionRank]
+    }
+    
+    // 参加者向けAPI
+    // GET /api/player/competition/:competition_id/ranking
+    // 大会ごとのランキングを取得する
+    func competitionRanking() async throws -> Response {
+        return try .json(content: SuccessResult())
+    }
 
-@main struct Main {
-    static func main() async throws {
-        LoggingSystem.bootstrap { label in
-            var handler = ConsoleLogger(label: label, console: Terminal())
-            handler.logLevel = .debug
-            return handler
-        }
-        
-        let (pool, shutdown) = connectAdminDB()
-        defer { try! shutdown.close() }
-        
-        let threadPool = NIOThreadPool(numberOfThreads: 16)
-        threadPool.start()
-        defer { try! threadPool.syncShutdownGracefully() }
-        
-        let app = try Application(.detect())
-        defer { app.shutdown() }
-        
-        app.http.server.configuration.port = Int(getEnv(key: "SERVER_APP_PORT", defaultValue: "3000"))!
-        app.middleware.use(CacheControlPrivateMiddleware())
-        
-        func jsonRoute(_ route: @escaping @Sendable (Handler.Type) -> ((Handler) -> () async throws -> some Encodable)) -> (Request) async throws -> Response {
-            { request in
-                let server = Handler(
-                    request: request,
-                    adminDB: pool.database(logger: request.logger),
-                    threadPool: threadPool,
-                    eventLoopGroup: request.eventLoop
-                )
-                let handler = route(Handler.self)
-                let result = try await handler(server)()
-                let response = Response()
-                try response.content.encode(result, as: .json)
-                return response
-            }
-        }
-        
-        app.post("/api/admin/tenants/add", use: jsonRoute { $0.tenantsAdd })
-        
-        try app.start()
-        try await app.running?.onStop.get()
+    struct CompetitionsResult: Encodable {
+        var competitions: [CompetitionDetail]
+    }
+    
+    // 参加者向けAPI
+    // GET /api/player/competitions
+    // 大会の一覧を取得する
+    func playerCompetitions() async throws -> Response {
+        return try .json(content: SuccessResult())
+    }
+    
+    // テナント管理者向けAPI
+    // GET /api/organizer/competitions
+    // 大会の一覧を取得する
+    func organizerCompetitions() async throws -> Response {
+        return try .json(content: SuccessResult())
+    }
+    
+    func competitions(viewer: Viewer, tenantDB: some SQLDatabase) async throws -> Response {
+        fatalError("TODO")
+    }
+    
+    struct TenantDetail: Encodable {
+        var name: String
+        var display_name: String
+    }
+
+    struct MeResult: Encodable {
+        var tenant: TenantDetail?
+        var me: PlayerDetail?
+        var role: Role
+        var logged_in: Bool
+    }
+    
+    // 共通API
+    // GET /api/me
+    // JWTで認証した結果、テナントやユーザ情報を返す
+    func me() async throws -> Response {
+        return try .json(content: SuccessResult())
+    }
+    
+    struct InitializeResult: Encodable {
+        var lang: String
+    }
+    
+    // ベンチマーカー向けAPI
+    // POST /initialize
+    // ベンチマーカーが起動したときに最初に呼ぶ
+    // データベースの初期化などが実行されるため、スキーマを変更した場合などは適宜改変すること
+    func initialize() async throws -> Response {
+        return try .json(content: SuccessResult())
     }
 }
 
@@ -409,5 +661,13 @@ final class SQLExecuteBuilder: SQLQueryFetcher, Sendable {
     init(query: SQLQueryString, database: any SQLDatabase) {
         self.query = query
         self.database = database
+    }
+}
+
+extension Response {
+    static func json(content: some Encodable) throws -> Response {
+        let response = Response()
+        try response.content.encode(content, as: .json)
+        return response
     }
 }
